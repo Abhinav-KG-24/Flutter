752b9a7801d884f6d49ec9cfb5194eff8b920c90
Abhinav kg <abhinavkg24@gmail.com>
2023-11-07 19:45:40 +0530

    hello world

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: Center( // this widget is usd for center the content
+        child: Text('hello world'), // text is using to add text content
+        ),
+        ),
+        );
 }
 
