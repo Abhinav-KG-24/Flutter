da408212241477db1f3d4f01b3bbf9e0a2d4add5
Abhinav kg <abhinavkg24@gmail.com>
2023-11-08 19:20:20 +0530

    fineshed i am rich app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Center( // this widget is usd for center the content
-        child: Text('hello world'), // text is using to add text content
-        ),
-        ),
-        );
+        home: Scaffold(
+            backgroundColor: const Color.fromARGB(255, 226, 101, 92),
+            appBar: AppBar(
+              title: Center(child: Text('I am rich')),
+              backgroundColor: const Color.fromARGB(255, 255, 17, 0),
+            ),
+            body: Center(
+              child: Image(image: AssetImage('images/img.png')),
+            ))),
+  );
 }
-
